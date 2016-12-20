5299fcfb84d8ee56ffb5cf849cc6dcde1e7e97ac
Leela Krishna Devalla <krishnadevalla@outlook.com>
2016-12-20 12:24:37 -0500

    Removed Unnecessary "Usings".

--- Clock WPF/MainWindow.xaml.cs
+++ Clock WPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Clock_WPF
 {
@@ -36,7 +26,7 @@ namespace Clock_WPF
                 return "Red";
             }
             else
-                return "Yellow";
+                return "White";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- Clock WPF/Models/Items.cs
+++ Clock WPF/Models/Items.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Clock_WPF.Models
+﻿namespace Clock_WPF.Models
 {
     class Item
     {
-        public String Text { get; set; }
+        public string Text { get; set; }
         public double Angle { get; set; }
         public double ReverseAngle { get { return -this.Angle; } }
 
